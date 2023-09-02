87f95c4f6ba63eb013dc2e5e3aa4b4b5e1f6fecd
Purna-ASU <ppeddire@asu.edu>
2023-09-01 17:34:56 -0700

    Added banner data

---
+++ src/assets/bannerData.js
@@ -0,0 +1,24 @@
+const bannerData = [
+    {
+        title: "Congratulations!",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.",
+        status: "success"
+    },
+    {
+        title: "Attention!",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.",
+        status: "warning"
+    },
+    {
+        title: "There is a problem with your application!",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.",
+        status: "error"
+    },
+    {
+        title: "Update available!",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.",
+        status: "neutral"
+    }
+]
+
+export default bannerData;
